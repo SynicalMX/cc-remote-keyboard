67f6fd9f83119316710beecde9ceb3edd444d7ca
SynicalMX <70176240+SynicalMX@users.noreply.github.com>
2023-02-18 12:11:54 +0000

    fix installer + add download retry

--- installer.lua
+++ installer.lua
@@ -3,7 +3,13 @@ local URL = "https://raw.githubusercontent.com/SynicalMX/cc-remote-keyboard/main
 local function downloadFile(path)
     local url = URL..path
 
-    local http_handle = http.get(url)
+    local http_handle, err = http.get(url)
+    if err then
+        print("Retrying download...")
+        sleep(0.5)
+        downloadFile(path)
+        return
+    end
     local file_handle = fs.open("/keyboard/"..path, "w")
 
     file_handle.write(http_handle.readAll())
@@ -13,5 +19,5 @@ local function downloadFile(path)
 end
 
 downloadFile("main.lua")
-downloadFile("drawings.lua")
+downloadFile("drawing.lua")
 downloadFile("buttons.lua")
